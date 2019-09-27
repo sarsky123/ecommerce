2a360ae340d5077aa2dcb203be85ce3aac1a7c12
Yu Lo <vikinglo123@gmail.com>
2019-09-27 12:50:38 +0800

    organized step 2, after creating pkg of server dir, inb4 installing dependencies

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,7 +5,6 @@ import * as cart from "@/store/modules/cart.js";
 import * as authentication from "@/store/modules/authentication.js";
 import * as notification from "@/store/modules/notification.js";
 import * as auth0 from "@/store/modules/auth0.js";
-import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
