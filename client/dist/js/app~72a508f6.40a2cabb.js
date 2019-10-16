3e83b6c54c89bd6d8475dbca7b72dd9179032ad9
Yu Lo <vikinglo123@gmail.com>
2019-10-16 14:11:00 +0800

    client deployed

--- client/dist/js/app~72a508f6.40a2cabb.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["app~72a508f6"],{f669:function(p,n,w){}}]);
-//# sourceMappingURL=app~72a508f6.40a2cabb.js.map
--- client/dist/js/chunk-vendors~ea246c04.7f42d798.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~ea246c04"],{"77ed":function(n,o,w){}}]);
-//# sourceMappingURL=chunk-vendors~ea246c04.7f42d798.js.map
