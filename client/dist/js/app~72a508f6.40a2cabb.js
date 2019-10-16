616288cc6232b92943cdf809a8f6a3b90b759a2b
Yu Lo <vikinglo123@gmail.com>
2019-10-16 13:52:55 +0800

    deployed to heroku

---
+++ client/dist/js/app~72a508f6.40a2cabb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["app~72a508f6"],{f669:function(p,n,w){}}]);
+//# sourceMappingURL=app~72a508f6.40a2cabb.js.map
---
+++ client/dist/js/chunk-vendors~ea246c04.7f42d798.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~ea246c04"],{"77ed":function(n,o,w){}}]);
+//# sourceMappingURL=chunk-vendors~ea246c04.7f42d798.js.map
